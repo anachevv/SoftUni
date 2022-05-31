91ceef780a7cb76ea2723dae78aa307cf8b0ec5a
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-05-31 18:07:00 +0300

    Added another solution

--- SoftUni-Project/SoftUni Fundamentals/Data Types and Variables - More Exercises/exchange_integers.py
+++ SoftUni-Project/SoftUni Fundamentals/Data Types and Variables - More Exercises/exchange_integers.py
@@ -1,8 +1,18 @@
+# a = int(input())
+# b = int(input())
+#
+# print(f'Before:\na = {a}\nb = {b}')
+#
+# a, b = b, a
+#
+# print(f'After:\na = {a}\nb = {b}')
+
+
 a = int(input())
 b = int(input())
 
-print(f'Before:\na = {a}\nb = {b}')
+tmp = a
+a = b
+b = tmp
 
-a, b = b, a
-
-print(f'After:\na = {a}\nb = {b}')
+print(f'Before:\na = {tmp}\nb = {a}\nAfter:\na = {a}\nb = {b}')
