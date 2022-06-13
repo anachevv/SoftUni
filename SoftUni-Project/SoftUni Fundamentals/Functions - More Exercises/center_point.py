33457b09e370a3bc132f1d1bd4cf6087898bc5f1
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-06-13 18:12:47 +0300

    Added a forgotten statement to the problem.

--- SoftUni-Project/SoftUni Fundamentals/Functions - More Exercises/center_point.py
+++ SoftUni-Project/SoftUni Fundamentals/Functions - More Exercises/center_point.py
@@ -2,10 +2,13 @@ from math import floor
 
 
 def center_point(x_one, y_one, x_two, y_two):
-    sum_of_first_list = [x_one, y_one]
-    sum_of_second_list = [x_two, y_two]
+    first_list = [x_one, y_one]
+    second_list = [x_two, y_two]
 
-    the_closest_points = sum_of_first_list if sum([abs(x_one), abs(y_one)]) < sum([abs(x_two), abs(y_two)]) else sum_of_second_list
+    first_sum = sum([abs(x_one), abs(y_one)])
+    second_sum = sum([abs(x_two), abs(y_two)])
+
+    the_closest_points = first_list if first_sum < second_sum else second_list if second_sum < first_sum else first_list
 
     return f'({floor(the_closest_points[0])}, {floor(the_closest_points[1])})'
 
