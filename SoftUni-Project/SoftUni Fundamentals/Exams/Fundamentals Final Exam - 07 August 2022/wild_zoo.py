a3b6351955489cb6e447b403fbb6b78701cef250
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-08-07 20:49:47 +0300

    All solutions added at once.

---
+++ SoftUni-Project/SoftUni Fundamentals/Exams/Fundamentals Final Exam - 07 August 2022/wild_zoo.py
@@ -0,0 +1,36 @@
+command = input()
+info = {}
+while command != "EndDay":
+    command = command.split(': ')
+    if 'Add' in command:
+        current_command = command[-1].split('-')
+        name, needed_food, area = current_command[0], int(current_command[1]), current_command[2]
+        if name not in info:
+            info[name] = [0, area]
+        info[name][0] += needed_food
+    elif 'Feed' in command:
+        current_command = command[-1].split('-')
+        name, food = current_command[0], int(current_command[1])
+        if name in info:
+            info[name][0] -= food
+            if info[name][0] <= 0:
+                print(f"{name} was successfully fed")
+                del info[name]
+    command = input()
+
+'''
+{'Adam': [4500, 'ByTheCreek'], 'Maya': [8830, 'WaterfallArea']}
+'''
+
+areas = {}
+print('Animals:')
+for name, info in info.items():
+    current_area = info[-1]
+    if current_area not in areas:
+        areas[current_area] = 0
+    areas[current_area] += 1
+    print(f" {name} -> {info[0]}g")
+
+print("Areas with hungry animals:")
+for area, number in areas.items():
+    print(f" {area}: {number}")
