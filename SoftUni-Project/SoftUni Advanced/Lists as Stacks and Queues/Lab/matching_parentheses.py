337a02036bc08147e18e44d87e82013202184c73
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-08-27 14:53:15 +0300

    quick update

--- SoftUni-Project/SoftUni Advanced/Lists as Stacks and Queues/Lab/matching_parentheses.py
+++ SoftUni-Project/SoftUni Advanced/Lists as Stacks and Queues/Lab/matching_parentheses.py
@@ -1,8 +1,8 @@
 string = input()
-lst = []
+results = []
 for index in range(len(string)):
     if string[index] == '(':
-        lst.append(index)
+        results.append(index)
     elif string[index] == ')':
-        start_index = lst.pop()
+        start_index = results.pop()
         print(string[start_index: index + 1])
