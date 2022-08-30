803b99fa13ca65fdcb66f3076965586f345d12f0
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-08-30 20:35:02 +0300

    Solution was updated

--- SoftUni-Project/SoftUni Advanced/Lists as Stacks and Queues/Lab/water_dispenser.py
+++ SoftUni-Project/SoftUni Advanced/Lists as Stacks and Queues/Lab/water_dispenser.py
@@ -1,12 +1,13 @@
+from collections import deque
+
 water_quantity = int(input())
 name = input()
-queue = []
+queue = deque()
 while name != 'Start':
     queue.append(name)
     name = input()
 
 command = input()
-person = 0
 while command != 'End':
     if 'refill' in command:
         liters_to_add = int(command.split()[1])
@@ -15,10 +16,10 @@ while command != 'End':
         liters_to_give = int(command)
         if liters_to_give <= water_quantity:
             water_quantity -= liters_to_give
-            print(f"{queue[person]} got water")
-            queue.remove(queue[person])
+            print(f"{queue[0]} got water")
+            queue.popleft()
         else:
-            print(f"{queue[person]} must wait")
-            queue.remove(queue[person])
+            print(f"{queue[0]} must wait")
+            queue.popleft()
     command = input()
 print(f"{water_quantity} liters left")
