81d2072790dc51ba0819845e4c5eb8bc577ade62
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-09-05 21:29:05 +0300

    Solution was updated.

--- SoftUni-Project/SoftUni Advanced/Tuples and Sets/Lab/summation_pairs.py
+++ SoftUni-Project/SoftUni Advanced/Tuples and Sets/Lab/summation_pairs.py
@@ -5,8 +5,9 @@ iterations = 0
 for first_number in range(len(numbers)):
     for second_number in range(first_number + 1, len(numbers)):
         iterations += 1
-        if first_number + second_number == target_number:
-            print(f"{first_number} + {second_number} = {target_number}")
-            unique_pairs.append((first_number, second_number))
+        if numbers[first_number] + numbers[second_number] == target_number:
+            print(f"{numbers[first_number]} + {numbers[second_number]} = {target_number}")
+            unique_pairs.append((numbers[first_number], numbers[second_number]))
 unique_pairs = set(unique_pairs)
 print(f"Iterations done: {iterations}")
+print('\n'.join(str(x) for x in unique_pairs))
