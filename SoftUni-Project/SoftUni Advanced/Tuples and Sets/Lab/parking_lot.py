26f630c82328e264f8f572cab6fa7a1e388e3824
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-09-05 19:25:27 +0300

    Solution was updated.

--- SoftUni-Project/SoftUni Advanced/Tuples and Sets/Lab/parking_lot.py
+++ SoftUni-Project/SoftUni Advanced/Tuples and Sets/Lab/parking_lot.py
@@ -4,8 +4,7 @@ for _ in range(n_commands):
     command = input().split(', ')
     direction, reg_number = command
     if direction == 'IN':
-        if reg_number not in reg_numbers:
-            reg_numbers.append(reg_number)
+        reg_numbers.append(reg_number)
     elif direction == 'OUT':
         if reg_number in reg_numbers:
             reg_numbers.remove(reg_number)
