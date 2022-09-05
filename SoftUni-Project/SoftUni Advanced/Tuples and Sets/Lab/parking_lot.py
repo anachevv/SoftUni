5a95de77c373dcc466032e2254e4e3483aa4b928
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-09-05 13:48:36 +0300

    A solution was added.

---
+++ SoftUni-Project/SoftUni Advanced/Tuples and Sets/Lab/parking_lot.py
@@ -0,0 +1,15 @@
+n_commands = int(input())
+reg_numbers = []
+for _ in range(n_commands):
+    command = input().split(', ')
+    direction, reg_number = command
+    if direction == 'IN':
+        if reg_number not in reg_numbers:
+            reg_numbers.append(reg_number)
+    elif direction == 'OUT':
+        if reg_number in reg_numbers:
+            reg_numbers.remove(reg_number)
+if set(reg_numbers):
+    print("\n".join(set(reg_numbers)))
+else:
+    print("Parking Lot is Empty")
