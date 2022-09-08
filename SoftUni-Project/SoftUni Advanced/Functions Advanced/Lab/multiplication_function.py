27aabee3644071d1c92a586991c34fe09c75a193
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-09-08 15:08:07 +0300

    A solution was added.

---
+++ SoftUni-Project/SoftUni Advanced/Functions Advanced/Lab/multiplication_function.py
@@ -0,0 +1,8 @@
+def multiply(*num):
+    result = 1
+    for i in num:
+        result *= i
+    return result
+print(multiply(1, 4, 5))
+print(multiply(4, 5, 6, 1, 3))
+print(multiply(2, 0, 1000, 5000))
