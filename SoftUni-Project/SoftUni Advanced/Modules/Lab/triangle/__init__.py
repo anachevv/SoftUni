e7603a69f8823682cbd089267694a4789ecc27c8
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-09-09 14:28:47 +0300

    Python package was added.

---
+++ SoftUni-Project/SoftUni Advanced/Modules/Lab/triangle/__init__.py
@@ -0,0 +1,10 @@
+def display_triangle(n):
+    for row in range(1, n + 1):
+        for i in range(1, row + 1):
+            print(i, end=" ")
+        print()
+
+    for row in range(n - 1, 0, -1):
+        for i in range(1, row + 1):
+            print(i, end=" ")
+        print()
