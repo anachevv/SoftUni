089917c973275059cc7c9f16374b621a037ac46a
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-09-09 12:27:25 +0300

    Updated.

--- SoftUni-Project/SoftUni Advanced/Error Handling/Lab/repeat_text.py
+++ SoftUni-Project/SoftUni Advanced/Error Handling/Lab/repeat_text.py
@@ -1,7 +1,7 @@
 text = input()
 try:
     n_times = int(input())
-    result = text * 2
+    result = text * n_times
     print(result)
 except ValueError:
     print("Variable times must be an integer")
