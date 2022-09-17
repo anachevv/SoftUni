46d99dd1890538fc3a996eaa5d4dfc80695cf941
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-09-17 12:14:42 +0300

    A solution was added.

--- SoftUni-Project/SoftUni Advanced/Stacks, Queues, Tuples and Sets - Exercise/expression_evaluator.py
+++ SoftUni-Project/SoftUni Advanced/Stacks, Queues, Tuples and Sets - Exercise/expression_evaluator.py
@@ -1,20 +1,20 @@
 from math import floor
 
-'''
-['1', '0', '2', '3', '*', '4', '2', '/', '3', '0', '1', '0', '+', '1', '0', '0', '5', '0', '-', '2', '-', '1', '*']
-'''
+
 def get_expr(expr):
     for index in range(len(expr)):
         if expr[index].isdigit() or len(expr[index]) > 1:
             expr[index] = int(expr[index])
     return expr
 
+
 def evaluator(expr):
+    expr = get_expr(expr)
     numbers = []
     result = int(expr.pop(0))
     for element in expr:
-        if element.isnumeric():
-            numbers.append(int(element))
+        if element not in ('*', '/', '+', '-'):
+            numbers.append(element)
         else:
             if element == '-':
                 for num in numbers:
@@ -32,6 +32,6 @@ def evaluator(expr):
             numbers = []
     return result
 
+
 expr = [x for x in input().split()]
 print(evaluator(expr))
-# ALMOST DONE!
