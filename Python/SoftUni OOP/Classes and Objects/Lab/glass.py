ddd9ccbadcb7f16752e431609cf867d7ee9c789c
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-10-27 16:52:19 +0300

    solution added

---
+++ Python/SoftUni OOP/Classes and Objects/Lab/glass.py
@@ -0,0 +1,27 @@
+class Glass:
+    capacity = 250
+
+    def __init__(self):
+        self.content = 0
+
+    def fill(self, quantity):
+        if self.content + quantity <= Glass.capacity:
+            self.content += quantity
+            return f"Glass filled with {quantity} ml"
+        return f"Cannot add {quantity} ml"
+
+    def empty(self):
+        self.content = 0
+        return "Glass is now empty"
+
+    def info(self):
+        space_left = Glass.capacity - self.content
+        return f"{space_left} ml left"
+
+
+glass = Glass()
+print(glass.fill(100))
+print(glass.fill(200))
+print(glass.empty())
+print(glass.fill(200))
+print(glass.info())
