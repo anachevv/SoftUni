a3c2545a3c7dc872e70fc2049a666a01a99ec557
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-11-19 18:56:54 +0200

    solution added

---
+++ Python/SoftUni OOP/SOLID/Exercise/workers-updated/main.py
@@ -0,0 +1,24 @@
+from project.manager import WorkManager, BreakManager
+from project.robot import Robot
+from project.super_worker import SuperWorker
+from project.worker import Worker
+
+work_manager = WorkManager()
+break_manager = BreakManager()
+work_manager.set_worker(Worker())
+break_manager.set_worker(Worker())
+work_manager.manage()
+break_manager.lunch_break()
+
+work_manager.set_worker(SuperWorker())
+break_manager.set_worker(SuperWorker())
+work_manager.manage()
+break_manager.lunch_break()
+
+work_manager.set_worker(Robot())
+work_manager.manage()
+try:
+    break_manager.set_worker(Robot())
+    break_manager.lunch_break()
+except:
+    pass
---
+++ Python/SoftUni OOP/SOLID/Exercise/workers-updated/project/robot.py
@@ -0,0 +1,7 @@
+from project.worker import Workable
+
+
+class Robot(Workable):
+
+    def work(self):
+        print("I'm a robot. I'm working....")
