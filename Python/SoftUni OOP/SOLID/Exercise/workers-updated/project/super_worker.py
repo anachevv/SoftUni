45904d06d42d198d1e8d090c9d6060e8fe8bfeaa
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-11-20 10:56:08 +0200

    solution updated

---
+++ Python/SoftUni OOP/SOLID/Exercise/workers-updated/project/super_worker.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+from project.worker import Workable, Eatable
+
+
+class SuperWorker(Workable, Eatable):
+
+    def work(self):
+        print("I'm super worker. I work very hard!")
+
+    def eat(self):
+        print("Lunch break....(3 secs)")
+        sleep(3)
---
+++ Python/SoftUni OOP/SOLID/Exercise/workers-updated/project/worker.py
@@ -0,0 +1,28 @@
+from abc import abstractmethod
+from time import sleep
+
+
+class Workable:
+    @abstractmethod
+    def work(self):
+        pass
+
+    @abstractmethod
+    def eat(self):
+        pass
+
+
+class Eatable:
+    @abstractmethod
+    def eat(self):
+        pass
+
+
+class Worker(Workable, Eatable):
+
+    def work(self):
+        print("I'm normal worker. I'm working.")
+
+    def eat(self):
+        print("Lunch break....(5 secs)")
+        sleep(5)
