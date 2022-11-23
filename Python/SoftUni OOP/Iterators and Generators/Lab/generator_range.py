e4931aea11685d34b55ff456020a618bd31be544
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-11-23 19:32:00 +0200

    quick update

--- Python/SoftUni OOP/Iterators and Generators/Lab/generator_range.py
+++ Python/SoftUni OOP/Iterators and Generators/Lab/generator_range.py
@@ -1,9 +1,3 @@
 def genrange(start: int, end: int):
-    num = start - 1
-
-    while num < end:
-        num += 1
+    for num in range(start, end + 1):
         yield num
-
-
-print(list(genrange(1, 10)))
