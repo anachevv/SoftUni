1ceb7fb0f17a7dccc41df5d2083b9d4270ff2fe5
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-11-26 22:28:41 +0200

    solution added

---
+++ JS/Fundamentals/Basics/Lab/theatrePromotions.js
@@ -0,0 +1,38 @@
+function tickets(dayType, age) {
+    var ticketPrice = 0;
+
+    if (age < 0 || age > 122) {
+        return "Error!";
+    }
+
+    if (age <= 18) {
+        if (dayType === "Weekday") {
+            ticketPrice = 12;
+        } else if (dayType === "Weekend") {
+            ticketPrice = 15;
+        } else if (dayType === "Holiday") {
+            ticketPrice = 5;
+        }
+    } else if (age <= 64) {
+        if (dayType === "Weekday") {
+            ticketPrice = 18;
+        } else if (dayType === "Weekend") {
+            ticketPrice = 20;
+        } else if (dayType === "Holiday") {
+            ticketPrice = 12;
+        }
+    } else if (age <= 122) {
+        if (dayType === "Weekday") {
+            ticketPrice = 12;
+        } else if (dayType === "Weekend") {
+            ticketPrice = 15;
+        } else if (dayType === "Holiday") {
+            ticketPrice = 10;
+        }
+    }
+
+    return ticketPrice + "$";
+
+}
+
+console.log(tickets('Holiday', 15));
