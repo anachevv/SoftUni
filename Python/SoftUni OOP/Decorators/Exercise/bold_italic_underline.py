9cc058d0cd951dce7c7be918e25092fe69a6a84e
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-12-03 20:38:19 +0200

    solution added

---
+++ Python/SoftUni OOP/Decorators/Exercise/bold_italic_underline.py
@@ -0,0 +1,32 @@
+def make_bold(function):
+    def wrapper(*args):
+        result = "<b>" + function(*args) + "</b>"
+        return result
+
+    return wrapper
+
+
+def make_italic(function):
+    def wrapper(*args):
+        result = "<i>" + function(*args) + "</i>"
+        return result
+
+    return wrapper
+
+
+def make_underline(function):
+    def wrapper(*args):
+        result = "<u>" + function(*args) + "</u>"
+        return result
+
+    return wrapper
+
+
+@make_bold
+@make_italic
+@make_underline
+def greet_all(*args):
+    return f"Hello, {', '.join(args)}"
+
+
+print(greet_all("Peter", "George"))
