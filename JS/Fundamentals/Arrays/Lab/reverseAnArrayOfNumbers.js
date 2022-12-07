bd46e20d9f0183501937e062a962ea7e942266c8
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-12-07 20:25:56 +0200

    solution added

---
+++ JS/Fundamentals/Arrays/Lab/reverseAnArrayOfNumbers.js
@@ -0,0 +1,20 @@
+function main(n, numbers) {
+    var newArray = [];
+    
+    for (let i = 0; i < numbers.length; i++) {
+        if (i === n) {
+            break;
+        }
+        newArray.push(numbers[i]);
+    }
+
+    newArray = newArray.reverse();
+
+    return newArray.join(" ").trim();
+}
+
+console.log(
+    main(
+        3, [10, 20, 30, 40, 50]
+    )
+)
